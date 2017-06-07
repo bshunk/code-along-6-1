0c94b61e70ae67c749103a57d4994f0ee4cd67bd
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-07 15:27:13 -0500

    transferred correct codeAlong docs

--- arrays.js
+++ arrays.js
@@ -1,36 +1,36 @@
-let colors = ["red", "blue", "green", "yellow", "orange", "teal"];
-let numbers = [20,10,5,8,30,100,19];
+// let colors = ["red", "blue", "green", "yellow", "orange", "teal"];
+// let numbers = [20,10,5,8,30,100,19];
 
-let reversedColors = colors.reverse()
-console.log("oooh, pretty", reversedColors);
+// let reversedColors = colors.reverse()
+// console.log("oooh, pretty", reversedColors);
 
-let sortedColors = colors.sort();
-console.log("sorted", sortedColors);
+// let sortedColors = colors.sort();
+// console.log("sorted", sortedColors);
 
-// BAD WAY TO SORT NUMBERS, ONLY SORTS THE FIRST CHARACTER//
-let sortaSorted = numbers.sort();
-console.log("uhhhh", sortaSorted);
+// // BAD WAY TO SORT NUMBERS, ONLY SORTS THE FIRST CHARACTER//
+// let sortaSorted = numbers.sort();
+// console.log("uhhhh", sortaSorted);
 
-let sortedNums = numbers.sort(function(first, second){
-	return first - second;
-});
-console.log("sortedNums", sortedNums);
+// let sortedNums = numbers.sort(function(first, second){
+// 	return first - second;
+// });
+// console.log("sortedNums", sortedNums);
 
-let joinedColors = colors.join(", ");
-console.log("joined", joinedColors);
+// let joinedColors = colors.join(", ");
+// console.log("joined", joinedColors);
 
-// SLICE CUTS FROM THE BEGINNING OF THE ARRAY YOU CHOOSE TO THE BEFORE POSITION OF THE CHARACTER AFTER THE DESIRED ONE//
-let fruits = ["banana", "orange", "lemon", "apple", "mango"];
-let citrus = fruits.slice(1, 3);
-// console.log("fruits", fruits);
-console.log("cirtus?", citrus);
+// // SLICE CUTS FROM THE BEGINNING OF THE ARRAY YOU CHOOSE TO THE BEFORE POSITION OF THE CHARACTER AFTER THE DESIRED ONE//
+// let fruits = ["banana", "orange", "lemon", "apple", "mango"];
+// let citrus = fruits.slice(1, 3);
+// // console.log("fruits", fruits);
+// console.log("cirtus?", citrus);
 
 
-// var colors = ["red", "blue", "green", "yellow", "orange", "teal"];
+// // var colors = ["red", "blue", "green", "yellow", "orange", "teal"];
 
-colors.forEach(function(color) {
-	console.log(`I like the color ${color}`);
-});
+// colors.forEach(function(color) {
+// 	console.log(`I like the color ${color}`);
+// });
 
 
 
--- objects.js
+++ objects.js
@@ -1,3 +1,63 @@
+const songMaker = {
+	makeSongString: function(){
+		return `"${song.title}" is from the album "${song.album}" by ${song.artist}`
+	},
+	makeSongCard: function() {
+		return `<div class="song-card"><h2></h2></div>`
+	}
+};
+
+let song1 = Object.create(songMaker);
+song1.title = "Private Idaho";
+song1.artist = "The B-52s";
+console.log("song1", song1);
+
+let song2 = Object.create(songMaker);
+song2.title = "Shiny Happy People";
+song2.artist = "REM";
+console.log("song2", song2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const song = {
 // 	title: "Private Idaho", 
 // 	artist: "The B-52s",
@@ -5,33 +65,13 @@
 // 	album: "Wild Planet",
 // 	bandMembers: [
 // 	{name:"Fred", roles: {vocals: "lead", instrument: null} }
-// 	// {"Cindy", roles: {vocals: "backup", instrument: null} }
+// 	// {"Cindy",
 // 	// {"Ricky",
 // 	// {"Keith",
 // 	// {"Kate"}
-// 	],
-// 	makeSongString: function() {
-// 		return `"${song.title}" is from the album "${song.album}" by ${song.artist}`
-// 	}	
+// 	],	
 // };
 
-const song = {
-	title: "Party Up",
-	artist: "DMX",
-	plays: 2000,
-	album: "Run With The Wolf",
-	bandMembers: [
-	{name:"DMX", roles: {vocals: "lead", instrument: null} }
-	// {"Rock",
-	// {"XZibit",
-	// {"DJ Khaled"}
-	],
-	makeSongString: function() {
-		return `"${song.title}" is from the alumb "${song.album}" by ${song.artist}`
-	}
-};
-
-console.log("The jam", song.makeSongString());
 
 // const member = song.bandMembers[0];
 // const vocalRole = member.roles.vocals;
@@ -41,47 +81,22 @@ console.log("The jam", song.makeSongString());
 // 	console.log(`${member.name} did not sing on this track`);
 // };
 
-const member = song.bandMembers[0];
-const vocalRole = member.roles.vocals;
-if(vocalRole) {
-	console.log(`${member.name} was the ${vocalRole} singer on this track` );
-} else {
-	console.log(`${member.name} did not sing on this track`);
-};
-
 
 // console.log("Fred's vocals", song.bandMembers[0].roles.vocals);
 // song["albumCoverColor"] = "red";
 // console.log("the artist", song["artist"]);
 
 
-console.log("DMX crushed it on the", song.bandMembers[0].roles.vocals);
-song["albumCoverColor"] = "red";
-console.log("the artist?", song["artist"]);
-
 // const record = "album";
 // console.log("album?", song[record]);
 // console.log(song.makeSongString());console.log("Fred's vocals", song.bandMembers[0].roles.vocals);
 // song["albumCoverColor"] = "red";
 // console.log("the artist", song["artist"]);
-// const record = "album";
-
-const record = "album";
-
-console.log("the album?", song[record]);
-
-console.log(song.makeSongString());console.log("DMX's vocals", song.bandMembers[0].roles.vocals);
-
-song["alumbCoverColor"] = "red";
-
-console.log("the artist is the legend", song["artist"]);
 
 
 // console.log("album?", song[record]);
 // console.log(song.makeSongString());
 
-console.log("album?", song[record]);
-console.log(song.makeSongString());
 
 
 
@@ -91,20 +106,121 @@ console.log(song.makeSongString());
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const song = {
+// 	title: "Party Up",
+// 	artist: "DMX",
+// 	plays: 2000,
+// 	album: "Run With The Wolf",
+// 	bandMembers: [
+// 	{name:"DMX", roles: {vocals: "lead", instrument: null} }
+// 	// {"Rock",
+// 	// {"XZibit",
+// 	// {"DJ Khaled"}
+// 	],
+// 	makeSongString: function() {
+// 		return `"${song.title}" is from the alumb "${song.album}" by ${song.artist}`
+// 	}
+// };
+
+
+// const member = song.bandMembers[0];
+// const vocalRole = member.roles.vocals;
+// if(vocalRole) {
+// 	console.log(`${member.name} was the ${vocalRole} singer on this track` );
+// } else {
+// 	console.log(`${member.name} did not sing on this track`);
+// };
+
+
+// console.log("DMX crushed it on the", song.bandMembers[0].roles.vocals);
+// song["albumCoverColor"] = "red";
+// console.log("the artist?", song["artist"]);
+
+
+// const record = "album";
+
+// console.log("the album?", song[record]);
+
+// console.log(song.makeSongString());console.log("DMX's vocals", song.bandMembers[0].roles.vocals);
+
+// song["alumbCoverColor"] = "red";
+
+// console.log("the artist is the legend", song["artist"]);
+
+// console.log("The jam", song.makeSongString());
+
+
+// console.log("album?", song[record]);
+// console.log(song.makeSongString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PROTOTYPE CHAIN=LINKING ONE OBJECT TO ANOTHER (SOMEHOW)
 
 
 
 
 // .REPEAT IS A METHOD THAT IS SUPER IMPORTANT!!!!!!!!!!!!	//
-
-
-
-
-
-
-
-
-
 
 
 
